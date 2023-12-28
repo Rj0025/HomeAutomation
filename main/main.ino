79c79ebd58584671b3fa79679b8351e920c1bd88
Arjay Lipio <aj@quxtech.tv>
2023-12-28 19:29:42 +0800

    Connect Firebase or Control With Switch

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,5 @@
 // Make it 1 to see all debug messages in Serial Monitor
-#define DEBUG_SW 1
+#define DEBUG_SW 0
 // Pins of Switches / Input
 #define S1 12  // D6
 #define S2 13  // D7
@@ -13,8 +13,6 @@
 #define R4 3   // RX
 #define R5 1   // TX
 
-// Wifi Led
-#define wifiLed D4
 // Insert RTDB URLefine the RTDB URL */
 #define FIREBASE_HOST ""
 // Insert Firebase project API Key
@@ -31,13 +29,11 @@
 #include "addons/RTDBHelper.h"
 
 // Function Declaration
-void with_internet();
-void without_internet();
-void getDataFirebaseData(String url, int relayPin);
-void setDataFirebaseData(String url, int *switchFlag, int switchPin, int relayPin);
-void readDataFromFirebase();
 void log(String message);
-void localSwitch(bool switchState, int *prevState, bool *relayState, int relayPin);
+void processData();
+void getDataFirebaseData(String url, int relayPin, bool *relayState);
+void readDataFromFirebase();
+void handleSwitchAction(bool switchState, int *prevState, bool *relayState, int relayPin, String url);
 
 // switch state
 int switchFlag1 = 0;
@@ -60,7 +56,7 @@ FirebaseConfig config;
 int intValue = 0;
 // Flag for syncing
 bool local = false;
-bool remote = false;
+bool isSync = false;
 
 void log(String message) {
   if (DEBUG_SW)
@@ -73,21 +69,19 @@ void setup() {
   pinMode(R1, OUTPUT);
   pinMode(R2, OUTPUT);
   pinMode(R3, OUTPUT);
-  // pinMode(R4, OUTPUT);
-  // pinMode(R5, OUTPUT);
+  pinMode(R4, OUTPUT);
+  pinMode(R5, OUTPUT);
   digitalWrite(R1, HIGH);
   digitalWrite(R2, HIGH);
   digitalWrite(R3, HIGH);
-  // digitalWrite(R4, HIGH);
-  // digitalWrite(R5, HIGH);
+  digitalWrite(R4, HIGH);
+  digitalWrite(R5, HIGH);
   // switch
   pinMode(S1, INPUT_PULLUP);
   pinMode(S2, INPUT_PULLUP);
   pinMode(S3, INPUT_PULLUP);
   pinMode(S4, INPUT_PULLUP);
   pinMode(S5, INPUT_PULLUP);
-  // led light
-  pinMode(wifiLed, OUTPUT);
   // WIFI
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   // Wait for wifi for 10 secs
@@ -105,7 +99,6 @@ void setup() {
   // Set up FirebaseAuth object
   config.signer.tokens.legacy_token = FIREBASE_AUTH;
   // Initialize Firebase with the config and auth objects
-  // Firebase.begin(&config, &auth);
   Firebase.begin(&config, &auth);
   delay(2000);
   log("Start Programming");
@@ -115,55 +108,56 @@ void loop() {
   if (WiFi.status() != WL_CONNECTED) {
     log("Wifi Not Connected");
     local = true;
-    without_internet();
+    isSync = false;
   } else {
-    // sync data from local to firebase
-    if (local == true) {
-      // Firebase.RTDB.setInt(&fbdo, "/kubo/light1/value", digitalRead(R1));
-      // Firebase.RTDB.setInt(&fbdo, "/kubo/light2/value", digitalRead(R2));
-      // Firebase.RTDB.setInt(&fbdo, "/kubo/light3/value", digitalRead(R3));
-      // Firebase.RTDB.setInt(&fbdo, "/kubo/light4/value", digitalRead(R4));
-      // Firebase.RTDB.setInt(&fbdo, "/kubo/outlet1/value", digitalRead(R5));
+    if (local) {
+      // sync data from local to firebase
+      Firebase.RTDB.setInt(&fbdo, "/kubo/light1/value", (relayState1) ? 1 : 0);
+      Firebase.RTDB.setInt(&fbdo, "/kubo/light2/value", (relayState2) ? 1 : 0);
+      Firebase.RTDB.setInt(&fbdo, "/kubo/light3/value", (relayState3) ? 1 : 0);
+      Firebase.RTDB.setInt(&fbdo, "/kubo/light4/value", (relayState4) ? 1 : 0);
+      Firebase.RTDB.setInt(&fbdo, "/kubo/outlet1/value", (relayState5) ? 1 : 0);
       local = false;
     }
     log("Wifi Connected");
     readDataFromFirebase();
-    with_internet();
   }
+  processData();
 }
 
-void getDataFirebaseData(String url, int relayPin) {
+void getDataFirebaseData(String url, int relayPin, bool *relayState) {
   if (Firebase.RTDB.getInt(&fbdo, url)) {
     if (fbdo.dataType() == "int") {
       intValue = fbdo.intData();
       log(intValue);
-      if (intValue == 1) {
-        digitalWrite(relayPin, LOW);
-      } else {
-        digitalWrite(relayPin, HIGH);
-      }
+      (*relayState) = (intValue == 1) ? HIGH : LOW;
+      digitalWrite(relayPin, (*relayState));
     } else {
       log(fbdo.errorReason());
     }
   }
 }
 
-void setDataFirebaseData(String url, int *switchFlag, int switchPin, int relayPin) {
-  if (digitalRead(switchPin) == LOW) {
-    if ((*switchFlag) == 0) {
-      log("Button Pressed");
-      if (digitalRead(relayPin) == LOW) {
-        digitalWrite(relayPin, HIGH);
-        if (Firebase.RTDB.setInt(&fbdo, url, 0)) {
-          log("PASSED");
-          log("PATH: " + fbdo.dataPath());
-        } else {
-          log("FAILED");
-          log("REASON: " + fbdo.errorReason());
-        }
-      } else {
-        digitalWrite(relayPin, LOW);
-        if (Firebase.RTDB.setInt(&fbdo, url, 1)) {
+void readDataFromFirebase() {
+  if (Firebase.ready()) {
+    getDataFirebaseData("/kubo/light1/value", R1, &relayState1);
+    getDataFirebaseData("/kubo/light2/value", R2, &relayState2);
+    getDataFirebaseData("/kubo/light3/value", R3, &relayState3);
+    getDataFirebaseData("/kubo/light4/value", R4, &relayState4);
+    getDataFirebaseData("/kubo/outlet1/value", R5, &relayState5);
+  }
+}
+
+void handleSwitchAction(bool switchState, int *prevState, bool *relayState, int relayPin, String url) {
+  if (switchState != (*prevState)) {
+    // Check if the switch is pressed (state changed from HIGH to LOW)
+    if (switchState == LOW) {
+      // Toggle the relay state
+      (*relayState) = !(*relayState);
+      digitalWrite(relayPin, (*relayState));  // Update the relay state
+      if (WiFi.status() == WL_CONNECTED && Firebase.ready()) {
+        int convertedValue = (*relayState) ? 1 : 0;
+        if (Firebase.RTDB.setInt(&fbdo, url, convertedValue)) {
           log("PASSED");
           log("PATH: " + fbdo.dataPath());
         } else {
@@ -171,167 +165,21 @@ void setDataFirebaseData(String url, int *switchFlag, int switchPin, int relayPi
           log("REASON: " + fbdo.errorReason());
         }
       }
-      (*switchFlag) = 1;
-    }
-  }
-  if (digitalRead(switchPin) == HIGH) {
-    if ((*switchFlag) == 1) {
-      // digitalWrite(relayPin, HIGH);
-      // if (Firebase.RTDB.setInt(&fbdo, url, 0)) {
-      //   log("PASSED");
-      //   log("PATH: " + fbdo.dataPath());
-      // } else {
-      //   log("FAILED");
-      //   log("REASON: " + fbdo.errorReason());
-      // }
-      (*switchFlag) = 0;
-      // log("Switch OFF");
-    }
-  }
-}
-
-void readDataFromFirebase() {
-  getDataFirebaseData("/kubo/light1/value", R1);
-  getDataFirebaseData("/kubo/light2/value", R2);
-  getDataFirebaseData("/kubo/light3/value", R3);
-  // getDataFirebaseData("/kubo/light4/value", R4);
-  // getDataFirebaseData("/kubo/outlet1/value", R5);
-}
-
-void with_internet() {
-  setDataFirebaseData("/kubo/light1/value", &switchFlag1, S1, R1);
-  setDataFirebaseData("/kubo/light2/value", &switchFlag2, S2, R2);
-  setDataFirebaseData("/kubo/light3/value", &switchFlag3, S3, R3);
-  // setDataFirebaseData("/kubo/light4/value", &switchFlag4, S4, R4);
-  // setDataFirebaseData("/kubo/outlet1/value", &switchFlag5, S5, R5);
-}
-
-void localSwitch(bool switchState, int *prevState, bool *relayState, int relayPin) {
-  if (switchState != (*prevState)) {
-    // Check if the switch is pressed (state changed from HIGH to LOW)
-    if (switchState == LOW) {
-      // Toggle the relay state
-      (*relayState) = !(*relayState);
-      digitalWrite(relayPin, (*relayState));  // Update the relay state
     }
   }
   (*prevState) = switchState;
 }
 
-void without_internet() {
+void processData() {
   bool switchState1 = digitalRead(S1);
   bool switchState2 = digitalRead(S2);
   bool switchState3 = digitalRead(S3);
   bool switchState4 = digitalRead(S4);
   bool switchState5 = digitalRead(S5);
 
-  localSwitch(switchState1, &switchFlag1, &relayState1, R1);
-  localSwitch(switchState2, &switchFlag2, &relayState2, R2);
-  localSwitch(switchState3, &switchFlag3, &relayState3, R3);
-  // localSwitch(switchState4, &switchFlag4, &relayState4, R4);
-  // localSwitch(switchState5, &switchFlag5, &relayState5, R5);
+  handleSwitchAction(switchState1, &switchFlag1, &relayState1, R1, "/kubo/light1/value");
+  handleSwitchAction(switchState2, &switchFlag2, &relayState2, R2, "/kubo/light2/value");
+  handleSwitchAction(switchState3, &switchFlag3, &relayState3, R3, "/kubo/light3/value");
+  handleSwitchAction(switchState4, &switchFlag4, &relayState4, R4, "/kubo/light3/value");
+  handleSwitchAction(switchState5, &switchFlag5, &relayState5, R5, "/kubo/light3/value");
 }
-
-
-
-// void Switch_Read()
-// {
-//   if (digitalRead(Switch1) == LOW)
-//   {
-//     Power = 0;
-//     update_power_status();
-//     load1 = !load1;
-//     Relays();
-//     if (Firebase.setString(firebaseData, "/L1", String(load1)))
-//     {
-//       Serial.println("PASSED");
-//       Serial.println("PATH: " + firebaseData.dataPath());
-//       Serial.println("TYPE: " + firebaseData.dataType());
-//       Serial.println("ETag: " + firebaseData.ETag());
-//       Serial.println("------------------------------------");
-//       Serial.println();
-//     }
-//     else
-//     {
-//       Serial.println("FAILED");
-//       Serial.println("REASON: " + firebaseData.errorReason());
-//       Serial.println("------------------------------------");
-//       Serial.println();
-//     }
-//     delay(300);
-//   }
-//   else if (digitalRead(Switch2) == LOW)
-//   {
-//     Power = 0;
-//     update_power_status();
-//     load2 = !load2;
-//     Relays();
-//     if (Firebase.setString(firebaseData, "/L2", String(load2)))
-//     {
-//       Serial.println("PASSED");
-//       Serial.println("PATH: " + firebaseData.dataPath());
-//       Serial.println("TYPE: " + firebaseData.dataType());
-//       Serial.println("ETag: " + firebaseData.ETag());
-//       Serial.println("------------------------------------");
-//       Serial.println();
-//     }
-//     else
-//     {
-//       Serial.println("FAILED");
-//       Serial.println("REASON: " + firebaseData.errorReason());
-//       Serial.println("------------------------------------");
-//       Serial.println();
-//     }
-//     delay(300);
-//   }
-
-//   else if (digitalRead(Switch3) == LOW)
-//   {
-//     Power = 0;
-//     update_power_status();
-//     load3 = !load3;
-//     Relays();
-//     if (Firebase.setString(firebaseData, "/L3", String(load3)))
-//     {
-//       Serial.println("PASSED");
-//       Serial.println("PATH: " + firebaseData.dataPath());
-//       Serial.println("TYPE: " + firebaseData.dataType());
-//       Serial.println("ETag: " + firebaseData.ETag());
-//       Serial.println("------------------------------------");
-//       Serial.println();
-//     }
-//     else
-//     {
-//       Serial.println("FAILED");
-//       Serial.println("REASON: " + firebaseData.errorReason());
-//       Serial.println("------------------------------------");
-//       Serial.println();
-//     }
-//     delay(300);
-//   }
-
-//   else if (digitalRead(Switch4) == LOW)
-//   {
-//     Power = 0;
-//     update_power_status();
-//     load4 = !load4;
-//     Relays();
-//     if (Firebase.setString(firebaseData, "/L4", String(load4)))
-//     {
-//       Serial.println("PASSED");
-//       Serial.println("PATH: " + firebaseData.dataPath());
-//       Serial.println("TYPE: " + firebaseData.dataType());
-//       Serial.println("ETag: " + firebaseData.ETag());
-//       Serial.println("------------------------------------");
-//       Serial.println();
-//     }
-//     else
-//     {
-//       Serial.println("FAILED");
-//       Serial.println("REASON: " + firebaseData.errorReason());
-//       Serial.println("------------------------------------");
-//       Serial.println();
-//     }
-//     delay(300);
-//   }
-// }
